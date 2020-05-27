3dbed700fc1dc7dabc6fece4bc280bff6ee0ca14
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-05-27 12:58:41 -0700

    feat: added ProjectContext.js

--- src/components/ProjectsPage/ProjectsPage.js
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -4,17 +4,26 @@ import { Button, Section } from '../Utils/Utils';
 
 
 export default class ProjectsPage extends Component {
+    // PostApiService.getMyProjects()
+    // .then(this.context.setMyProjects)
+    // .catch(this.context.setError)
+
+
   render() {
+    // const myProjects = this.context.myProjects;
+
     return (
       <Section className="projects-page">
         <Button className="start-project" onClick={e => {e.preventDefault(); this.props.history.push('/project-form')}}>Create a new project</Button>
         <h2>Projects i'm involved in:</h2>
-        <ul>
-          <li>Test Project1</li>
-          <li>Test Project2</li>
-          <li>Test Project3</li>
-          <li>Test Project4</li>
-        </ul>
+       {/* {myProjects.map(project => {
+         <div>
+           <h3>project.name</h3>
+           <p>project.description</p>
+           <p>project.tags</p>
+           <p>project.links</p>
+         </div>
+       })} */}
       </Section>
     )
   }
