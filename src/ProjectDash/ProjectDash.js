789280a47245be5d14a12c37e3d06359557d2be3
Sara R Mills <SaraRMills@gmail.com>
2020-05-27 13:51:36 -0400

    feat: project dashboard working rendering properly with dummy data, making changes to user_role in state results in different elements being rendered

--- src/ProjectDash/ProjectDash.js
+++ src/ProjectDash/ProjectDash.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 // import projectdashservice from './projectdashservice'
 import './ProjectDash.css';
+import { project, vacancies, requests } from './dummydata';
 
 class ProjectDash extends Component {
   //user can be project_owner, team_member, or user
   state = {
-    user_role: 'user'
+    user_role: 'team_member',
+    project,
+    vacancies,
+    requests
   };
+
+  componentDidMount() {
+    //wanna get all the project and request data here, store it in state, also get user role and store that as well
+  }
 
   handleLeaveTeam = e => {
     e.preventDefault();
@@ -40,94 +48,119 @@ class ProjectDash extends Component {
   };
 
   render() {
+    let { project, vacancies, requests, user_role } = this.state;
+
+    let tagsList = project.tags.map(tag => {
+      return <li key={tag}>{tag}</li>;
+    });
+
+    let vacancyList = vacancies.map(item => {
+      return (
+        <li key={item.id}>
+          <p>
+            {item.user_id === null ? 'This role is available' : item.user_id}
+          </p>
+          <p>Role: {item.title}</p>
+          <p>Duties: {item.description}</p>
+          <p>Skills: {item.skills.join(', ')}</p>
+        </li>
+      );
+    });
+
+    let requestList = requests.map(request => {
+      return (
+        <li key={request.id}>
+          {request.name} wants to join your team
+          <button onClick={this.handleDecline} type="button">
+            Decline
+          </button>
+          <button onClick={this.handleApprove} type="button">
+            Approve
+          </button>
+          <button onClick={this.handleNewMessage} type="button">
+            Message
+          </button>
+        </li>
+      );
+    });
+
     return (
       <section className="ProjectDash">
         <h1>Project Dashboard</h1>
         <article className="ProjectDash__project">
-          <h2>Project Title</h2>
-          <p>project description</p>
-          <ul className="ProjectDash__tags">
-            <li>Tag1</li>
-            <li>Tag2</li>
-            <li>Tag3</li>
-          </ul>
-          <ul className="ProjectDash__vacancies">
-            <p>not sure all the info that needs to be shown here</p>
-            <li>vacancy1</li>
-            <li>vacancy2</li>
-            <li>vacancy3</li>
-          </ul>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+          <ul className="ProjectDash__tags">{tagsList}</ul>
+          <ul className="ProjectDash__vacancies">{vacancyList}</ul>
         </article>
-        <button type="button" onClick={this.handleRequest}>
-          Request to join
-        </button>
-
-        <article className="ProjectDash__team-options">
-          <a
-            className="ProjectDash__links"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://www.github.com"
-          >
-            Github
-          </a>
-          <a
-            className="ProjectDash__links"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://www.github.com"
-          >
-            Live Page
-          </a>
-          <a
-            className="ProjectDash__links"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://www.trello.com"
-          >
-            Trello
-          </a>
-
-          <form
-            onSubmit={this.handlePost}
-            className="ProjectDash__post-to-project"
-          >
-            <label htmlFor="ProjectDash__create-post">
-              What do you want to post?
-            </label>
-            <input
-              name="ProjectDash__create-post"
-              id="ProjectDash__create-post"
-              type="text"
-              required
-            />
-            <button type="submit">Submit Post</button>
-          </form>
-          <button onClick={this.handleLeaveTeam} type="button">
-            Leave Team
+        {user_role === 'user' ? (
+          <button type="button" onClick={this.handleRequest}>
+            Request to join
           </button>
-        </article>
+        ) : (
+          ''
+        )}
 
-        <article className="ProjectDash__creator-options">
-          <div className="ProjectDash__pending-requests">
-            <ul className="ProjectDash__request">
-              <li>
-                Micheal Scott wants to join your team
-                <button onClick={this.handleDecline} type="button">
-                  Decline
-                </button>
-                <button onClick={this.handleApprove} type="button">
-                  Approve
-                </button>
-                <button onClick={this.handleNewMessage} type="button">
-                  Message
-                </button>
-              </li>
-            </ul>
-          </div>
-          <button type="button">Add new vacancy</button>
-          <button type="button">Delete this project</button>
-        </article>
+        {user_role === 'team_member' ? (
+          <article className="ProjectDash__team-options">
+            <a
+              className="ProjectDash__links"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.github.com"
+            >
+              Github
+            </a>
+            <a
+              className="ProjectDash__links"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.github.com"
+            >
+              Live Page
+            </a>
+            <a
+              className="ProjectDash__links"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.trello.com"
+            >
+              Trello
+            </a>
+
+            <form
+              onSubmit={this.handlePost}
+              className="ProjectDash__post-to-project"
+            >
+              <label htmlFor="ProjectDash__create-post">
+                What do you want to post?
+              </label>
+              <input
+                name="ProjectDash__create-post"
+                id="ProjectDash__create-post"
+                type="text"
+                required
+              />
+              <button type="submit">Submit Post</button>
+            </form>
+            <button onClick={this.handleLeaveTeam} type="button">
+              Leave Team
+            </button>
+          </article>
+        ) : (
+          ''
+        )}
+        {user_role === 'owner' ? (
+          <article className="ProjectDash__creator-options">
+            <div className="ProjectDash__pending-requests">
+              <ul className="ProjectDash__request">{requestList}</ul>
+            </div>
+            <button type="button">Add new vacancy</button>
+            <button type="button">Delete this project</button>
+          </article>
+        ) : (
+          ''
+        )}
       </section>
     );
   }
--- src/ProjectDash/dummydata.js
+++ src/ProjectDash/dummydata.js
@@ -1,13 +1,30 @@
-const projects = {
+const project = {
+  name: 'project 1',
+  description: 'stuff about this project is cool',
+  tags: ['react', 'css', 'javascript'],
+  github_url: 'https://github.com/thinkful-ei-macaw/dev-lft'
+};
 
-}
+const requests = [
+  { id: 1, name: 'Michael Scott' },
+  { id: 2, name: 'Pam Beesly' }
+];
 
-const requests = {
+const vacancies = [
+  {
+    id: 2,
+    title: 'Front-end Developer',
+    user_id: null,
+    description: 'build client stuff',
+    skills: ['React', 'Javascript', 'CSS']
+  },
+  {
+    id: 3,
+    title: 'Back-end Developer',
+    user_id: 'filled position',
+    description: 'build server stuff',
+    skills: ['Node', 'Javascript', 'Express']
+  }
+];
 
-}
-
-const vacancies = {
-
-}
-
-export default {projects, requests, vacancies}
+export { project, requests, vacancies };
