6bd60afcf00fc217abb2be1c0e9f03cb7821fbe6
atwb21786 <60189305+atwb21786@users.noreply.github.com>
2020-05-29 08:36:52 -0700

    feat: sign up page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import ProjectForm from './components/ProjectForm/ProjectForm';
 import ProjectsPage from './components/ProjectsPage/ProjectsPage';
 import FeedPage from './components/FeedPage/FeedPage';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 import UserProfile from './components/UserProfile/UserProfile';
 import Chat from './components/Chat/Chat';
 import ChatMessages from './components/ChatMessages/ChatMessages';
-import Login from './components/Login/Login';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Switch>
+          <Route path={'/signup'} component={Signup} />
           <Route path={'/login'} component={Login} />
           <Route path={'/my-projects'} component={ProjectsPage} />
           <Route path={'/project-form'} component={ProjectForm} />
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -41,9 +41,7 @@ class Login extends React.Component {
         <form onSubmit={this.handleJWTSubmission}>
           <fieldset>
             <h3>Sign In</h3>
-            <div role='alert'>
-          {error && <p>{error}</p>}
-        </div>
+            <div role="alert">{error && <p>{error}</p>}</div>
             <label htmlFor="username">username:</label>
             <input type="text" id="username" name="user_name" required />
             <br />
