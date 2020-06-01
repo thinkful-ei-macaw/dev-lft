764927b3f780fe48aa112372961d17cbe5f0bf6f
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-06-01 13:48:45 -0700

    feat: smoke tests for frontend components, 14 of 17 completed

---
+++ src/components/Chat/Chat.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chat from './Chat';
+import { BrowserRouter } from 'react-router-dom';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <Chat />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
---
+++ src/components/Footer/Footer.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Footer from './Footer';
+import { BrowserRouter } from 'react-router-dom';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
