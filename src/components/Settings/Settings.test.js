52609fb6fc8c3b96e63c68e097ad043f4e4fe136
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-06-02 12:25:07 -0700

    feat: add settings test

--- src/components/Settings/Settings.test.js
+++ src/components/Settings/Settings.test.js
@@ -5,9 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  const props = {
+    user_id:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwiaWF0IjoxNTgzNzI0MzAwLCJleHAiOjE1ODM4MTA3NTV9.fGA40guZ5zA4dy73bxqVh5zOPg4PLaFQQW31CpNJDbc'
+  };
   ReactDOM.render(
     <BrowserRouter>
-      <Settings />
+      <Settings {...props} />
     </BrowserRouter>,
     div
   );
