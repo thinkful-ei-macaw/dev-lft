642051b3e6b0676a5dc1a03e67a95858f5c795f7
Malcolm Kiano <malcolmkiano@gmail.com>
2020-06-04 16:35:43 -0400

    feat: update nav styling and logic

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,54 +4,86 @@ import './Nav.css';
 
 import UserContext from '../../contexts/UserContext';
 
+// images
+import { Logo } from '../../images/'
+
 export default class Nav extends Component {
   static contextType = UserContext;
 
+  renderLinks(links) {
+    const currentPath = this.props.location.pathname;
+    return links.map(({ text, path }) => (
+      <li key={path}>
+        <Link to={path} className={path === currentPath ? 'active' : ''}>{text}</Link>
+      </li>
+    ))
+  }
+
   renderPublicLinks() {
+    const publicLinks = [
+      { text: 'Sign Up', path: '/signup' },
+      { text: 'Login', path: '/login' }
+    ]
+
     return (
-      <ul className="links">
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <Link to="/login">Log In</Link>
-        </li>
+      <ul className="links links-right">
+        {this.renderLinks(publicLinks)}
       </ul>
     );
   }
 
   renderPrivateLinks() {
+    const privateLinks = [
+      { text: 'Feed', path: '/feed' },
+      { text: 'Projects', path: '/my-projects' },
+      { text: 'Chats', path: '/chats' }
+    ]
+
+    const rightLinks = [
+      { text: 'Settings', path: '/settings' }
+    ]
+
     return (
-      <ul className="links">
-        <li>
-          <Link to="/my-projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/chats">Chats</Link>
-        </li>
-        <li>
-          <Link to="/settings">Settings</Link>
-        </li>
-        <li>
-          <Link to="/" onClick={this.context.onLogOut}>
-            Log Out
+      <React.Fragment>
+        <ul className="links links-left">
+          {this.renderLinks(privateLinks)}
+        </ul>
+
+        <ul className="links links-right">
+          {this.renderLinks(rightLinks)}
+          <li>
+            <Link to="/" onClick={this.context.onLogOut}>
+              Log Out
           </Link>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </React.Fragment>
     );
   }
 
   render() {
     const { user } = this.context;
-    const homeURL = user ? '/feed' : '/';
+
+    // the nav bar is absolutely positioned
+    // this variable being `true` will render
+    // a div push the rest of the content down
+    // if user is on any private page (as per design)
+    const push = user.isAuth;
 
     return (
-      <nav>
-        <div className="wrapper">
-          <Link to={homeURL}>Dev LFT</Link>
-          {user.isAuth ? this.renderPrivateLinks() : this.renderPublicLinks()}
-        </div>
-      </nav>
+      <React.Fragment>
+        <nav>
+          <div className="wrapper">
+            <Link to='/'><Logo /></Link>
+            {
+              user.isAuth
+                ? this.renderPrivateLinks()
+                : this.renderPublicLinks()
+            }
+          </div>
+        </nav>
+        {push ? <div className="nav-push"></div> : ''}
+      </React.Fragment>
     );
   }
 }
