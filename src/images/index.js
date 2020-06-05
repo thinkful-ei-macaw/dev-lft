2c59d5e995d1b6e527fa8bcc4b104aec2b90a233
Malcolm Kiano <malcolmkiano@gmail.com>
2020-06-04 21:08:51 -0400

    fix: export footer icons

--- src/images/index.js
+++ src/images/index.js
@@ -1,3 +1,6 @@
 // this exports SVGs as React Components so they can be used inline
 // allows blazing-fast page loads
-export { ReactComponent as Logo } from './logo.svg';
+export { ReactComponent as Logo } from './svg/logo.svg';
+export { ReactComponent as FacebookIcon } from './svg/facebook.svg';
+export { ReactComponent as TwitterIcon } from './svg/twitter.svg';
+export { ReactComponent as LinkedInIcon } from './svg/linkedin.svg';
