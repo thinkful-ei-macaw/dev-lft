95a2c70f03006195b8e9f7b3a89fd1607e218c2b
Malcolm Kiano <malcolmkiano@gmail.com>
2020-06-06 14:32:33 -0400

    fix: update project form test with required props

--- src/components/ProjectForm/ProjectForm.test.js
+++ src/components/ProjectForm/ProjectForm.test.js
@@ -4,10 +4,11 @@ import ProjectForm from './ProjectForm';
 import { BrowserRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
+  const stub = () => null;
   const div = document.createElement('div');
   ReactDOM.render(
     <BrowserRouter>
-      <ProjectForm />
+      <ProjectForm onCreate={stub} onCancel={stub} />
     </BrowserRouter>,
     div
   );
