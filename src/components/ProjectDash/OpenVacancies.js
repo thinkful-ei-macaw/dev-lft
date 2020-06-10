b09f4081f5ac04a6a8c90718c4c9d3b11280283f
Malcolm Kiano <malcolmkiano@gmail.com>
2020-06-10 14:49:25 -0400

    feat: new vacancy error handling

--- src/components/ProjectDash/OpenVacancies.js
+++ src/components/ProjectDash/OpenVacancies.js
@@ -124,7 +124,7 @@ class OpenVacancies extends Component {
 
   render() {
     const { userRole } = this.props;
-    const { addingVacancy } = this.state;
+    const { addingVacancy, error } = this.state;
     return (
       <article className="card">
         <header className="title">
@@ -143,6 +143,7 @@ class OpenVacancies extends Component {
 
         {addingVacancy
           ? <VacancyModal
+            error={error}
             onSubmitVacancy={this.handleSubmitVacancy}
             onCloseVacancyModal={this.onCancelVacancy}
           />
